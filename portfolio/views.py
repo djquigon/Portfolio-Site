1803027c1116db246e043089da0343455766e460
Logan <lds96184@uga.edu>
2020-08-03 03:53:41 -0400

    added url routing for links in navbar

--- portfolio/views.py
+++ portfolio/views.py
@@ -7,3 +7,31 @@ def home(request):
     }
 
     return render(request, 'home.html', context)
+
+def software_work(request):
+    context = {
+
+    }
+
+    return render(request, 'software_work.html', context)
+
+def other_work(request):
+    context = {
+
+    }
+
+    return render(request, 'other_work.html', context)
+
+def about_me(request):
+    context = {
+
+    }
+
+    return render(request, 'about_me.html', context)
+
+def contact_me(request):
+    context = {
+
+    }
+
+    return render(request, 'contact_me.html', context)
