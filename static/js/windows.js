851cf3b114d60049363abe23e452c80d980547dc
Logan <lds96184@uga.edu>
2021-05-20 20:14:32 -0400

    fixed dos fullscreen issue and added eject game functionality by using newer js-dos version, updated desktop icons

--- static/js/windows.js
+++ static/js/windows.js
@@ -1,43 +1,36 @@
 function ejectGame(){
-    alert("Whoops! The eject game button doesn't work currently. Sorry about that, to exit the game simply refresh the page.")
-    // to be implemented
-    //disable buttons
+    //exit game
+    ci.exit()
+    //update ui
+    $("#jsdos").css('display', 'none')
+    $("#game").attr("class", "no-game");
+    $("#game-title").text("Pick a game from the Games folder in the Start menu");
+    $('.dosbox-button').prop('disabled', true);
 }
 
-function updateGame(game){
-    //close start menu
+function startGame(game){
     if($('#game').hasClass('no-game') == false){
-        alert("Sorry, you need to eject the current game before you can start another one.");
+        alert("You need to eject the current game before you can start another one.");
         return;
     }
     $("#game").attr("class", game);
     $("#game-title").text(game);
     $("#menu").css('display', 'none');
-    var dosbox = new Dosbox({
-        id: "dosbox",
-        onload: function (dosbox) {
-        dosbox.run('/static/assets/games/' + game + '.zip', "./" + game + ".EXE");
-        /*dosbox.run("{% static '/assets/games/DOOM.zip' %}", "./DOOM.EXE");*/
-        /*dosbox.run("{% static '/assets/games/MK3.zip' %}", "./MK3.EXE");*/
-        /*dosbox.run("{% static '/assets/games/DARKFORCES.zip' %}", "./DF.EXE");*/
-        /*dosbox.run("{% static '/assets/games/DUKE3D.zip' %}", "./DUKE3D.EXE");*/
-        /*dosbox.run("{% static '/assets/games/SUPERSTREETFIGHTER2T.zip' %}", "./SF2TURBO.EXE");*/
-        /*dosbox.run("{% static '/assets/games/SYSTEMSHOCK.zip' %}", "./SSHOCK.EXE");*/
-        /*dosbox.run("{% static '/assets/games/TETRIS.zip' %}", "./SABA.EXE");*/
-        /*dosbox.run("{% static '/assets/games/WOLF3D.zip' %}", "./WOLF3D.EXE");*/
-        /*dosbox.run("{% static '/assets/games/PACMAN.zip' %}", "./MSPAC.EXE");*/
-        /*dosbox.run("{% static '/assets/games/SOLITAIRE.zip' %}", "./SOLSUITE.EXE");*/
-        /*dosbox.run("{% static '/assets/games/MARIO.zip' %}", "./MARIO.EXE");*/
-        },
-        onrun: function (dosbox, app) {
-        console.log("App '" + app + "' is being ran");
-        }
-    });
-
-    $(".dosbox-start").text("Click to start " + game)
-    $(".dosbox-powered a").text("Powered by js-dos.com")
-    $(".dosbox-start").css('display', 'block');
-    $(".dosbox-powered a").css('display', 'block');
+    $("#startbutton").attr("class", "startbutton-off");
+    // fix start button toggle
+    $("#jsdos").css('display', 'inline-block')
+    Dos(document.getElementById("jsdos"), { 
+        wdosboxUrl: "https://js-dos.com/6.22/current/wdosbox.js",
+        //maybe max?
+        cycles: "auto",
+        autolock: false,
+      }).ready(function (fs, main) {
+        fs.extract("/static/assets/games/" + game + ".zip").then(function () {
+          main(["-c", "cd " + game, "-c", game + ".EXE"]).then(function (ci) {
+            window.ci = ci;
+          });
+        });
+      });
     //un disable buttons
     $('.dosbox-button').prop('disabled', false);
 }
